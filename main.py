afcec7d1af0d6d771f46f2466f9ce73d544ffd32
Anthony van Weel <82299803+AnthonyvW@users.noreply.github.com>
2023-02-24 20:20:51 -0900

    Added 3D visualizer

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ if(__name__ ==  "__main__"):
     pygame.init()  # Initialize Pygame Modules
 
     # Window Initialization
-    SCREEN_HEIGHT = 500
-    SCREEN_WIDTH = 500
+    SCREEN_HEIGHT = 520
+    SCREEN_WIDTH = 1020
     SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     # Set Window Name
     pygame.display.set_caption('Robot Simulation')
@@ -14,18 +14,21 @@ if(__name__ ==  "__main__"):
     FPS = 180
     CLOCK = pygame.time.Clock()
 
-    s = -1 # Arm orientation
-    l = 100 # Arm Segment Length
+    l1 = 75 # Arm Segment Length
+    l2 = 100
 
     # Base Position
-    baseX = 250
-    baseY = 250
+    baseX = 260
+    baseY = 260
+    baseZ = 0
     # Elbow Position
     elbowX = 0
     elbowY = 0
+    elbowZ = 0
     # End of Arm position
     handX = 100
     handY = 100
+    handZ = 0
 
 
     run = True
@@ -39,10 +42,10 @@ if(__name__ ==  "__main__"):
         destY = mousePos[1]
         if(
             # Restricts arm length to real bounds
-            math.sqrt((handX - baseX) ** 2 + (handY - baseY) ** 2) < l + l
+            math.sqrt((handX - baseX) ** 2 + (handY - baseY) ** 2) < l1 + l2
             or
             # Allows repositioning when mouse is moved back in range
-            math.sqrt((destX - baseX) ** 2 + (destY - baseY) ** 2) < l + l
+            math.sqrt((destX - baseX) ** 2 + (destY - baseY) ** 2) < l1 + l2
         ):
             # Move hand towards destination
             if(destX < handX):
@@ -61,14 +64,14 @@ if(__name__ ==  "__main__"):
 
             # Inner max is to prevent a divide by 0 error
             # Clamp the angle between the base and hand between the range of -1, 1
-            a = max(-1, min(1, d / max((2 * l * math.pow(d, 0.5)), 0.001)))
+            a = max(-1, min(1, (d + l1**2 - l2**2) / max((2 * l1 * math.pow(d, 0.5)), 0.001)))
             # Angle from elbow to hand
             # X angle - Y angle * arm orientation
-            t = math.atan2(p, c) - math.acos(a) * s
+            t = math.atan2(p, c) - math.acos(a) * -1
 
             # Displace elbow according to angle to hand
-            elbowX = handX + l * math.cos(t)
-            elbowY = handY + l * math.sin(t)
+            elbowX = handX + l1 * math.cos(t)
+            elbowY = handY + l1 * math.sin(t)
 
         # Handle Keypresses
         for event in pygame.event.get():  # Keypresses
@@ -85,11 +88,21 @@ if(__name__ ==  "__main__"):
         # Rendering
         SCREEN.fill((0, 0, 0))
         scaling = 1
-        pygame.draw.line(SCREEN, (255, 255, 255), (elbowX * scaling, elbowY * scaling), (handX * scaling, handY * scaling), 2)
+        # X / Y Plane
+        pygame.draw.rect(SCREEN,   (50, 50, 50), (10,10,SCREEN_WIDTH/2 - 15, SCREEN_HEIGHT - 20))
+        pygame.draw.line(SCREEN,   (255, 255, 255), (elbowX * scaling, elbowY * scaling), (handX * scaling, handY * scaling), 2)
         pygame.draw.circle(SCREEN, (255, 0, 0), (handX * scaling, handY * scaling), 8)
-        pygame.draw.line(SCREEN, (255, 255, 255), (elbowX * scaling, elbowY * scaling), (baseX * scaling, baseY * scaling), 2)
-        pygame.draw.circle(SCREEN, (255, 255, 0), (baseX * scaling, baseY * scaling), 8)
-        pygame.draw.circle(SCREEN, (255, 255, 255), (elbowX * scaling, elbowY * scaling), 8)
+        pygame.draw.line(SCREEN,   (255, 255, 255), (elbowX * scaling, elbowY * scaling), (baseX * scaling, baseY * scaling), 2)
+        pygame.draw.circle(SCREEN, (0, 0, 255), (baseX * scaling, baseY * scaling), 8)
+        pygame.draw.circle(SCREEN, (0, 255, 0), (elbowX * scaling, elbowY * scaling), 8)
+        # Z / Y Plane
+        ZYOffset = SCREEN_WIDTH/2 + 265
+        pygame.draw.rect(SCREEN, (50, 50, 50), (SCREEN_WIDTH/2 + 5, 10, SCREEN_WIDTH/2 - 15, SCREEN_HEIGHT - 20))
+        pygame.draw.circle(SCREEN, (0, 0, 255), (ZYOffset + baseZ * scaling, baseY * scaling), 8)
+        pygame.draw.line(SCREEN, (255, 255, 255), (ZYOffset + elbowZ * scaling, elbowY * scaling),(ZYOffset + handZ * scaling, handY * scaling), 2)
+        pygame.draw.circle(SCREEN, (0, 255, 0), (ZYOffset + elbowZ * scaling, elbowY * scaling), 8)
+        pygame.draw.line(SCREEN, (255, 255, 255), (ZYOffset + elbowZ * scaling, elbowY * scaling),(ZYOffset + baseZ * scaling, baseY * scaling), 2)
+        pygame.draw.circle(SCREEN, (255, 0, 0), (ZYOffset + handZ * scaling, handY * scaling), 8)
 
         pygame.display.update()  # Updates Screen
 
