3ab8e4e2d0c1256d48bc391e38bb2fd655daaaf6
Dawson-Packer <Dawson_Packer@outlook.com>
2024-03-18 19:17:23 -0800

    First serial

---
+++ SerialComms.py
@@ -0,0 +1,35 @@
+import serial as serial
+
+
+class SerialComms:
+    def __init__(self) -> None:
+        self.baudrate: int = None
+        self.port: str = None
+        self.ser: serial.Serial = None
+
+    def with_baudrate(self, baud: int) -> 'SerialComms':
+        self.baudrate = baud
+        return self
+    
+    def with_port(self, port: str) -> 'SerialComms':
+        self.port = port
+        return self
+    
+    def connect(self) -> bool:
+        if self.baudrate is None or self.port is None: raise IOError
+        self.ser = serial.Serial(
+            self.port,
+            self.baudrate,
+            8,
+            "N",
+        )
+        try:
+            self.ser.open()
+        except serial.SerialException as e:
+            print(e)
+        
+        self.ser.write("Hi from the mothership!")
+
+# Driver code
+        
+ser = SerialComms().with_baudrate(115200).with_port("/dev/cu.usbserial-141430").connect()
