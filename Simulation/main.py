5ece3de7733eb57911a7f44a32698c7025d2b7d9
Wa Xiong <waxiong77@gmail.com>
2024-03-26 18:05:23 -0800

    I got the bot to move but not like that. And when it moves slightly, the whole bot tips over. So I decided to lock the base onto the floor, but now the bot won't move at all. I've been unable to make the bot move again. Also had to change one of the joints from 'continuous' to 'revolute' so that an ikpy function would work. On that note, I'm using ikpy to get the positions, then just moving it using pybullet.

--- Simulation/main.py
+++ Simulation/main.py
@@ -2,6 +2,9 @@ import pybullet as p
 import time
 import pybullet_data
 
+#new imports
+import ikpy.chain
+
 physics_client = p.connect(p.GUI)#or p.DIRECT for non-graphical version
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
 p.setGravity(0,0,-10)
@@ -14,13 +17,50 @@ start_pos = [0,0,0]
 start_orientation = p.getQuaternionFromEuler([0,0,0])
 robot = p.loadURDF("robot.urdf",start_pos, start_orientation)
 
+# Stick Bot to Ground
+ground_joint = p.createConstraint(plane_ID, -1, 
+                                  robot, -1, 
+                                  jointType = p.JOINT_FIXED, 
+                                  jointAxis = [0,0,0], 
+                                  parentFramePosition = [-0.0274562034602567, -0.00137265400595656, 0.031237920982338], 
+                                  childFramePosition = [0.0274562034602567, 0.00137265400595656, -0.031237920982338])
+
+
 
 # Map joint names (from urdf) to index values "joint_L1" -> "joint_L6"
-joints = {}  
+joints = {}
 
 for i in range(p.getNumJoints(robot)):
     joint_name = p.getJointInfo(robot, i)[1].decode('UTF-8') 
     joints[joint_name] = i
+
+
+"""
+Link Activity:
+-FALSE = base_link
+-TRUE = link_L1
+- joint_L2
+-TRUE = link_L2
+- joint_L2
+-TRUE = link_L3
+- joint_L3
+-TRUE = link_L4
+- joint_L4
+-TRUE = link_L5
+- joint_L5
+-TRUE = link_L6
+- joint_L6
+
+"""
+
+
+chain = ikpy.chain.Chain.from_urdf_file("robot.urdf", active_links_mask = [False, True, True, True, True, True, True])
+target_pos = [2, 2, 2] # change
+target_orn = [-1, 0, 0]  # change
+
+ik = chain.inverse_kinematics(target_pos, target_orn, orientation_mode = "X") # returns list of position of each joint
+
+
 
 
 # Zoom in w/ camera
@@ -30,32 +70,34 @@ p.resetDebugVisualizerCamera(cameraDistance=1,
                              cameraTargetPosition=[0, 0, 0])
 
 
+# Set incrementation
+joint_1_inc = ik[1].item() / 10.0
+joint_2_inc = ik[2].item() / 10.0
+joint_3_inc = ik[3].item() / 10.0
+joint_4_inc = ik[4].item() / 10.0
+joint_5_inc = ik[5].item() / 10.0
+joint_6_inc = ik[6].item() / 10.0
 
-# Initial Position
-position = 0.0  
-
-last_time = time.time()
-INCREMENT_INTERVAL = 0.1  # Time in seconds to wait before next increment
-POSITION_INCREMENT = 0.05  # Amount by which to increase the joint position
-
-# Increment all joint(s) by POSITION_INCREMENT every INCREMENT_INTERVAL
-# Bounds of rotation limited by urdf
 while True:
 
-    current_time = time.time()
+    # move joints towards position
+    # p.setJointMotorControl2(robot, joints["joint_L1"], p.POSITION_CONTROL, joint_1_inc)
+    # p.setJointMotorControl2(robot, joints["joint_L2"], p.POSITION_CONTROL, joint_2_inc)
+    # p.setJointMotorControl2(robot, joints["joint_L3"], p.POSITION_CONTROL, joint_3_inc)
+    # p.setJointMotorControl2(robot, joints["joint_L4"], p.POSITION_CONTROL, joint_4_inc)
+    # p.setJointMotorControl2(robot, joints["joint_L5"], p.POSITION_CONTROL, joint_5_inc)
+    # p.setJointMotorControl2(robot, joints["joint_L6"], p.POSITION_CONTROL, joint_6_inc)
 
-    # Check if it's time to update the joint position
-    if current_time - last_time >= INCREMENT_INTERVAL:
-        position += POSITION_INCREMENT
 
-        # Increments position of 6th joint
-        p.setJointMotorControl2(robot, joints["joint_L6"], p.POSITION_CONTROL, targetPosition=position)
-
-        last_time = current_time
+    p.setJointMotorControl2(robot, joints["joint_L1"], p.POSITION_CONTROL, ik[1].item())
+    p.setJointMotorControl2(robot, joints["joint_L2"], p.POSITION_CONTROL, ik[2].item())
+    p.setJointMotorControl2(robot, joints["joint_L3"], p.POSITION_CONTROL, ik[3].item())
+    p.setJointMotorControl2(robot, joints["joint_L4"], p.POSITION_CONTROL, ik[4].item())
+    p.setJointMotorControl2(robot, joints["joint_L5"], p.POSITION_CONTROL, ik[5].item())
+    p.setJointMotorControl2(robot, joints["joint_L6"], p.POSITION_CONTROL, ik[6].item())
 
     p.stepSimulation()
 
-    # Sleep to simulate real time (this can be adjusted or removed as needed)
-    time.sleep(1/240)
+    time.sleep(1/50)
 
 p.disconnect()
