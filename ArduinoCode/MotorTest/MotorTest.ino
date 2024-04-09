76d71eadc56ddc2b8a84e9d8db1be879f9aaefbe
Mya Schroder <80082701+schromya@users.noreply.github.com>
2024-04-08 20:06:11 -0800

    Successfully spun continuosly and one rev

--- ArduinoCode/MotorTest/MotorTest.ino
+++ ArduinoCode/MotorTest/MotorTest.ino
@@ -1,28 +1,3 @@
-// void setup() {
-//   pinMode(5, OUTPUT); // Set the PWM pin as an output
-// }
-
-// void loop() {
-//   analogWrite(5, 128); // Generate a PWM signal with 50% duty cycle on pin 9
-//   // No need to change the value in loop() if you want a constant PWM signal
-// }
-
-// const int stepPin = 4; // Step pin
-// const int dirPin = 5;  // Direction pin
-
-// void setup() {
-//   pinMode(stepPin, OUTPUT);
-//   pinMode(dirPin, OUTPUT);
-// }
-
-// void loop() {
-//   digitalWrite(dirPin, HIGH); // Set the direction
-//   digitalWrite(stepPin, HIGH); // Move one step
-//   delay(1); // Wait
-//   digitalWrite(stepPin, LOW); // Pull step pin low so the motor can prepare for the next step
-//   delay(1); // Wait
-//   // Add code to turn off or change direction as needed
-// }
 
 // https://youtu.be/p4334ADfdF4?si=Z9UwGLWrCInvMCsT
 const int stepPin = 5;
@@ -32,11 +7,20 @@ void setup() {
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
   digitalWrite(dirPin, HIGH); // Set the direction
+
+  // Turns 1 full rev when encoders set tto 200 pulses/rev
+  // for (int i = 0; i< 200; i++) {
+  //   digitalWrite(stepPin, HIGH);
+  //   delayMicroseconds(50); // You may need to adjust this delay
+  //   digitalWrite(stepPin, LOW);
+  //   delayMicroseconds(1);
+
+  // }
 }
 
 void loop() {
-  digitalWrite(stepPin, HIGH);
-  delayMicroseconds(50); // You may need to adjust this delay
-  digitalWrite(stepPin, LOW);
-  delayMicroseconds(50);
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(100); // You may need to adjust this delay
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(100);
 }
