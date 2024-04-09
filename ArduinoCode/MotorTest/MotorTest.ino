dc6e29aac7fcb494a2c3928f8577c7501f144204
Mya Schroder <80082701+schromya@users.noreply.github.com>
2024-04-08 21:07:35 -0800

    Added both motors spinning simultaneously

--- ArduinoCode/MotorTest/MotorTest.ino
+++ ArduinoCode/MotorTest/MotorTest.ino
@@ -1,26 +1,33 @@
+#include <AccelStepper.h>
 
-// https://youtu.be/p4334ADfdF4?si=Z9UwGLWrCInvMCsT
-const int stepPin = 5;
-const int dirPin = 6;
+
+#define step_pin_6 5  // PUL
+#define dir_pin_6 4  // Direction 
+
+#define step_pin_5 7
+#define dir_pin_5 6
+
+#define steps_per_rev 200
+
+// Initialize AccelStepper for two motors
+AccelStepper stepper_6(AccelStepper::DRIVER, step_pin_6, dir_pin_6);
+AccelStepper stepper_5(AccelStepper::DRIVER, step_pin_5, dir_pin_5);
 
 void setup() {
-  pinMode(stepPin, OUTPUT);
-  pinMode(dirPin, OUTPUT);
-  digitalWrite(dirPin, HIGH); // Set the direction
+  // Set the maximum speed and acceleration:
+  stepper_6.setMaxSpeed(200);  // steps/second
+  stepper_6.setAcceleration(500); // steps/second^2
+  stepper_5.setMaxSpeed(200);
+  stepper_5.setAcceleration(500);
 
-  // Turns 1 full rev when encoders set tto 200 pulses/rev
-  // for (int i = 0; i< 200; i++) {
-  //   digitalWrite(stepPin, HIGH);
-  //   delayMicroseconds(50); // You may need to adjust this delay
-  //   digitalWrite(stepPin, LOW);
-  //   delayMicroseconds(1);
-
-  // }
+  // Set the target positions
+  stepper_6.moveTo(2 * steps_per_rev); // 2 Rev
+  stepper_5.moveTo(3 * steps_per_rev); // 3 Rev
 }
 
 void loop() {
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(100); // You may need to adjust this delay
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(100);
+  // These functions continuously move the stepper motors to their target positions
+  stepper_6.run();
+  stepper_5.run();
 }
+
