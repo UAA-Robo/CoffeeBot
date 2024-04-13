4df8d7207ed4a6c4cb09f341f82ee974c4b45727
Mya Schroder <80082701+schromya@users.noreply.github.com>
2024-04-12 16:46:49 -0800

    Updated ports

--- ArduinoCode/MotorTest/MotorTest.ino
+++ ArduinoCode/MotorTest/MotorTest.ino
@@ -1,11 +1,12 @@
 #include <AccelStepper.h>
+#define dir_pin_5 10
+#define step_pin_5 11
+
+#define dir_pin_6 12  // Direction
+#define step_pin_6 13  // PUL
+ 
 
 
-#define step_pin_6 5  // PUL
-#define dir_pin_6 4  // Direction 
-
-#define step_pin_5 7
-#define dir_pin_5 6
 
 #define steps_per_rev 200
 
@@ -15,9 +16,9 @@ AccelStepper stepper_5(AccelStepper::DRIVER, step_pin_5, dir_pin_5);
 
 void setup() {
   // Set the maximum speed and acceleration:
-  stepper_6.setMaxSpeed(200);  // steps/second
+  stepper_6.setMaxSpeed(400);  // steps/second
   stepper_6.setAcceleration(500); // steps/second^2
-  stepper_5.setMaxSpeed(200);
+  stepper_5.setMaxSpeed(400);
   stepper_5.setAcceleration(500);
 
   // Set the target positions
